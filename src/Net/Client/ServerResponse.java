35bf6e4dce1e8b415c7a2b91b00fbeec0fc238a2
DVN <237294@via.dk>
2017-05-22 20:12:08 +0200

    Fixed Netserver

--- src/Net/Client/ServerResponse.java
+++ src/Net/Client/ServerResponse.java
@@ -9,27 +9,27 @@ import Net.DataReceivedAction;
 import Net.NetMessage;
 import Net.SocketHandler;
 
-public class ServerResponse implements DataReceivedAction  {
+public class ServerResponse implements DataReceivedAction {
 	private int id;
 	private Object[] response;
 	private ResponseStatus status;
 	private LocalDateTime timeout;
-	
+
 	public ServerResponse(int id, int timeout) {
 		this.id = id;
 		setTimeout(timeout);
 	}
-	
+
 	public Object[] awaitResponse() throws InterruptedException {
-		while(status == ResponseStatus.InProgress && LocalDateTime.now().isBefore(timeout))
+		while (status == ResponseStatus.InProgress && LocalDateTime.now().isBefore(timeout))
 			Thread.sleep(1);
 		return response;
 	}
-	
+
 	public ResponseStatus getStatus() {
 		return status;
 	}
-	
+
 	public void setTimeout(int timeout) {
 		this.timeout = LocalDateTime.now().plusNanos((long) (timeout * 1e6));
 	}
@@ -37,26 +37,27 @@ public class ServerResponse implements DataReceivedAction  {
 	@Override
 	public void dataReceived(SocketHandler socket, NetMessage net) {
 		Map<String, Object> params = net.getParams();
-		
-		if(params.containsKey("REQUEST_ID"))
-		{
-			int reqID = (int)params.get("REQUEST_ID");
-			if(reqID == this.id)
-			{
-				//the response i was looking for.
-				socket.removeDataReceivedListener(this); //so i don't care about further messages
-				response = getParameters(params);
-				status = ResponseStatus.Ready;
+
+		if (net.getMessage().equals("RESPONSE")) {
+			if (params.containsKey("REQUEST_ID")) {
+				int reqID = (int) params.get("REQUEST_ID");
+				if (reqID == this.id) {
+					// the response i was looking for.
+					socket.removeDataReceivedListener(this); // so i don't care
+																// about further
+																// messages
+					response = getParameters(params);
+					status = ResponseStatus.Ready;
+				}
 			}
 		}
 	}
-	private Object[] getParameters(Map<String, Object> params)
-	{
+
+	private Object[] getParameters(Map<String, Object> params) {
 		ArrayList<Object> temp = new ArrayList<Object>();
-		for(int i = 0; true; i++)
-		{
-			String key = "PARAM" + Integer.toString(i);
-			if(params.containsKey(key))
+		for (int i = 0; true; i++) {
+			String key = "RESP" + Integer.toString(i);
+			if (params.containsKey(key))
 				temp.add(params.get(key));
 			else
 				break;
--- src/Net/Server/NetServer.java
+++ src/Net/Server/NetServer.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 import Net.DataReceivedAction;
 import Net.NetMessage;
@@ -49,7 +50,42 @@ public class NetServer implements Runnable, DataReceivedAction {
 	
 	@Override
 	public void dataReceived(SocketHandler socket, NetMessage net) {
-		
+		if(methodHandlers.containsKey(net.getMessage()))
+		{
+			if(net.getParams().containsKey("REQUEST_ID"))
+			{
+				int reqID = (int)net.getParams().get("REQUEST_ID");
+				Object[] methodReturn = methodHandlers.get(net.getMessage()).ServerMethod(getParameters(net.getParams()));
+				
+				//now send back the reply:
+				Map<String, Object> responseParams = new HashMap<String, Object>();
+				responseParams.put("REQUEST_ID", reqID);
+				for(int i = 0; i < methodReturn.length; i++)
+					responseParams.put("RESP" + Integer.toString(i), methodReturn[i]);
+				NetMessage response = new NetMessage("RESPONSE", responseParams);
+				try {
+					socket.send(response);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					return;
+				}
+				
+			}
+		}
+	}
+	private Object[] getParameters(Map<String, Object> params)
+	{
+		ArrayList<Object> temp = new ArrayList<Object>();
+		for(int i = 0; true; i++)
+		{
+			String key = "PARAM" + Integer.toString(i);
+			if(params.containsKey(key))
+				temp.add(params.get(key));
+			else
+				break;
+		}
+		return temp.toArray(new Object[0]);
 	}
 	
 }
