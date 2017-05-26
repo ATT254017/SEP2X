76e510f078c3025e8a58f2b8b5d23d867d6f1130
Afonso <254017@via.dk>
2017-05-26 15:27:00 +0200

    Implemented the JDBC

--- src/Server/DBControl.java
+++ src/Server/DBControl.java
@@ -1,8 +1,18 @@
 package Server;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Created by Afonso on 5/26/2017.
  */
 public class DBControl {
-
+	public static void main(String[] args) throws SQLException {
+		DriverManager.registerDriver(new org.postgresql.Driver());
+		Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres",
+				"password");
+		
+			connection.close();
+	}
 }
