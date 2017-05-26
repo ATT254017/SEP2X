e862489f7d4c07637d0759039d872a77723e8b7e
Afonso <254017@via.dk>
2017-05-26 18:38:23 +0200

    Added Tag class

---
+++ src/Model/Tag.java
@@ -0,0 +1,20 @@
+package Model;
+
+/**
+ * Created by Afonso on 5/26/2017.
+ */
+public class Tag {
+    private String tag;
+
+    public Tag(String tag) {
+        this.tag = tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+}
