ba912875da06f76ffdbfac211467a4076664bfd9
Afonso <254017@via.dk>
2017-05-28 15:26:44 +0200

    Added some more methods to the Method class and implemented the Image class

---
+++ src/Model/Image.java
@@ -0,0 +1,20 @@
+package Model;
+
+/**
+ * Created by Afonso on 5/28/2017.
+ */
+public class Image {
+    private String description;
+
+    public Image(String description) {
+        this.description = description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
